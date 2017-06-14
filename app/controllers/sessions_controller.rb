aa28591846e1fcc2425fb8fff1fe9c5a89c3c1eb
Jason Sears <jsears4585@gmail.com>
2017-06-14 14:46:43 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+require 'byebug'
+
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    return redirect_to sessions_new_path if params[:name].nil? || params[:name].empty?
+    session[:name] = params[:name]
+    redirect_to :root
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to :root
+  end
+end
